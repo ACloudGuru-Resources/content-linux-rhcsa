678e0b1db58bd32b70cc143d953d98246b17afa3
Marcos Monteiro <marcos-monteiro@pluralsight.com>
2024-01-29 11:53:14 +1100

    Update mkfs check message

--- vdo.sh
+++ vdo.sh
@@ -2,7 +2,7 @@
 [[ $( sudo vgdisplay | grep vdoDev > /dev/null 2>&1 ) ]] || sudo vgcreate vdoDev /dev/nvme1n1 /dev/nvme2n1 > /dev/null 2>&1
 [[ $( sudo lvdisplay | grep vdoLV > /dev/null 2>&1 ) ]] || sudo lvcreate -n vdoLV -l 100%FREE vdoDev > /dev/null 2>&1
 [[ $( sudo vdo list | grep LA > /dev/null 2>&1 ) ]] || sudo vdo create --name=LA --device=/dev/vdoDev/vdoLV --vdoLogicalSize=150G --sparseIndex=enabled > /dev/null 2>&1
-sudo blkid | grep LA | grep xfs > /dev/null 2>&1 || echo "Please wait for your lab environment to be fully provisioned, it may take between 5-10 minutes..."
+sudo blkid | grep LA | grep xfs > /dev/null 2>&1 || echo && echo "Please wait for your lab environment to be fully provisioned, it may take up to 5 minutes..." && echo
 [[ $( sudo blkid | grep LA | grep xfs > /dev/null 2>&1 ) ]] || sudo mkfs.xfs /dev/mapper/LA > /dev/null 2>&1
 [[ $( ls -d /mnt/vdo  > /dev/null 2>&1 ) ]] || sudo mkdir /mnt/vdo > /dev/null 2>&1
 [[ $( sudo mount | grep LA > /dev/null 2>&1 ) ]] || sudo mount /dev/mapper/LA /mnt/vdo > /dev/null 2>&1
